f2e0a50be75226dec69a607ed5ff42f50dbbbf38
Mahdi khayamdar <48985789+Mahdikh75@users.noreply.github.com>
2021-04-30 15:29:38 +0430

    Initial commit

---
+++ PortSerial/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PortSerial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Connetion = new System.Windows.Forms.Button();
+            this.DisConnetion = new System.Windows.Forms.Button();
+            this.Send = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Connetion
+            // 
+            this.Connetion.Location = new System.Drawing.Point(169, 12);
+            this.Connetion.Name = "Connetion";
+            this.Connetion.Size = new System.Drawing.Size(106, 23);
+            this.Connetion.TabIndex = 0;
+            this.Connetion.Text = "Connetion";
+            this.Connetion.UseVisualStyleBackColor = true;
+            this.Connetion.Click += new System.EventHandler(this.Connetion_Click);
+            // 
+            // DisConnetion
+            // 
+            this.DisConnetion.Location = new System.Drawing.Point(169, 41);
+            this.DisConnetion.Name = "DisConnetion";
+            this.DisConnetion.Size = new System.Drawing.Size(106, 23);
+            this.DisConnetion.TabIndex = 1;
+            this.DisConnetion.Text = "DisConnetion";
+            this.DisConnetion.UseVisualStyleBackColor = true;
+            this.DisConnetion.Click += new System.EventHandler(this.DisConnetion_Click);
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(169, 159);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(106, 23);
+            this.Send.TabIndex = 2;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 70);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(243, 73);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(69, 14);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(84, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ports";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 202);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.DisConnetion);
+            this.Controls.Add(this.Connetion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connetion;
+        private System.Windows.Forms.Button DisConnetion;
+        private System.Windows.Forms.Button Send;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PortSerial/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PortSerial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public delegate void MyDelegate();
+        public void UpdateTextBox()
+        {
+            richTextBox1.AppendText(serialPort1.ReadExisting());
+            richTextBox1.ScrollToCaret();
+        }
+        private void DateReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            richTextBox1.BeginInvoke(new MyDelegate(UpdateTextBox));
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] Prot = System.IO.Ports.SerialPort.GetPortNames();
+            for (int i = 1; i < Prot.Length; i++)
+            {
+                comboBox2.Items.Add(Prot[i]);
+            }
+            DisConnetion.Enabled = false;
+        }
+        private void Connetion_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen==true)
+            {
+                serialPort1.Close();
+            }
+            try
+            {
+                serialPort1.PortName = comboBox2.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Parity = System.IO.Ports.Parity.None;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                // Open The SerialPort1
+                serialPort1.Open();
+                // Update the 
+                richTextBox1.Text = comboBox2.Text + " Connetion ";
+                Connetion.Enabled = false;
+                DisConnetion.Enabled = true;
+            }
+            catch 
+            {
+                MessageBox.Show(" Error Serial Port Not Connetion  "," Error Port ",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+        }
+        private void DisConnetion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Close Serial Port
+                serialPort1.Close();
+                // Update Text richTextBox 
+                richTextBox1.Text = serialPort1.PortName + " DisConneted. ";
+                Connetion.Enabled = true;
+                DisConnetion.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show(" The Serial Port Not DisConneted ", " Error Dis Cooneted ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void Send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ///...(Rig &) Open key F7_(+"Shift"+"Tab"+;_;+"Ctrl") {~[!(+ Hack.IO & ^ % 7(MyComputer.PingID))]}=>;
+                ///...Wirte Date String Serial Port1; ;
+                serialPort1.Write(textBox1.Text + Environment.NewLine);
+                // Apped richTextbox 
+                richTextBox1.AppendText(" > " + textBox1.Text + Environment.NewLine);
+                richTextBox1.ScrollToCaret();
+                textBox1.Text = string.Empty; 
+            }
+            catch 
+            {
+                MessageBox.Show(" The Not Write Date String Serial Port ", " Error Send String ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
